3130e6801e4f0f8a0fbf1be1884d7163aa1ee88e
marcmasferrer00-ux <marcmasferrer00@gmail.com>
2025-10-03 19:08:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // script.js
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".hamburger");
-  const nav = document.querySelector("nav");
+  const nav = document.querySelector("header nav"); // 🔑 només el nav del header
 
   hamburger.addEventListener("click", () => {
     nav.classList.toggle("active");
   });
 
   // Tanquem menú quan fem clic a un enllaç
-  const links = document.querySelectorAll("nav ul li a");
+  const links = nav.querySelectorAll("ul li a");
   links.forEach(link => {
     link.addEventListener("click", () => {
       nav.classList.remove("active");
